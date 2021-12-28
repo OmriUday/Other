ef7f75e14ab055e305d441fad1ab2b98461c0974
Omri Uday <93121703+OmriUday@users.noreply.github.com>
2021-12-28 10:11:38 +0200

    Delete JS_HTML_CSS-Appleseeds-Bootcamp-Exercises-j/Week exercises directory

--- JS_HTML_CSS-Appleseeds-Bootcamp-Exercises-j/Week exercises/21.3 - text area/Solution/app.js
+++
@@ -1,5 +0,0 @@
-const submit = document.getElementById('submit');
-let input = document.getElementById('text');
-let p = document.querySelector('p');
-p.addEventListener('input', (updateValue) => {
-});
