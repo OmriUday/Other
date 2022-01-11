29e96588af6020932b25940f3621b7c3276936fb
Omri Uday <93121703+OmriUday@users.noreply.github.com>
2022-01-11 19:04:43 +0200

    Delete JS Weekend 2 directory

--- JS Weekend 2/Solution/Ex6_4__isogram.js
+++
@@ -1,7 +0,0 @@
-function indPerimeter(length, width) {
-  return (length + width) * 2;
-}
-//test
-console.log(indPerimeter(6, 7));
-console.log(indPerimeter(20, 10));
-console.log(indPerimeter(2, 9));
