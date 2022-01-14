7e31a8fd2052efcbdfa4aaa22ae05b49ab9fdcb2
Omri Uday <93121703+OmriUday@users.noreply.github.com>
2022-01-14 23:05:13 +0200

    Add files via upload

---
+++ JS Weekend 2/Solution/Ex8_Perimeter of a Rectangle.js
@@ -0,0 +1,7 @@
+function indPerimeter(length, width) {
+  return (length + width) * 2;
+}
+//test
+console.log(indPerimeter(6, 7));
+console.log(indPerimeter(20, 10));
+console.log(indPerimeter(2, 9));
