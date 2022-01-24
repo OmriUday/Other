f3c9ecbebb35e692ee565dd881bfed25b7b95561
Omri Uday <93121703+OmriUday@users.noreply.github.com>
2022-01-24 16:44:05 +0200

    Delete JS-Weekend-1--Exercises--Appleseeds-Bootcamp-main directory

--- JS-Weekend-1--Exercises--Appleseeds-Bootcamp-main/Solution/Ex2.1 - Sum of lowest numbers.js
+++
@@ -1,9 +0,0 @@
-function sumSmallest(arr) {
-  let small1 = Math.min(...arr);
-  arr.splice(arr.indexOf(small1), 1);
-  let small2 = Math.min(...arr);
-  arr.indexOf(small2);
-  console.log((small1) + (small2));
-}
-//test
-sumSmallest([0, 8, 11, 43, 90, 9, 6, 18, 3]);
