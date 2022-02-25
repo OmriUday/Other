109ecc7ec7baa3c2f73d454269d4305655709082
Omri Uday <93121703+OmriUday@users.noreply.github.com>
2022-02-25 18:40:26 +0200

    Delete -node-exercises-main/node-bootcamp directory

--- -node-exercises-main/node-bootcamp/Solutions/09-01/2.1/app.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs')
-fs.writeFileSync('notes.txt', 'My name is Omri.')
-fs.appendFileSync('notes.txt', ' And my last name is Uday')
-fs.promises.copyFile('./notes.txt', './fileCopied')
-fs.promises.rename('fileCopied', 'newFile')
-console.log(fs.readdirSync("./"));
