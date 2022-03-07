ff24f27911140a394ce440800231203c3b9e44f3
Omri Uday <93121703+OmriUday@users.noreply.github.com>
2022-03-07 22:34:22 +0200

    Add files via upload

---
+++ -node-exercises-main/node-bootcamp/codeAlong/get Jason from File/serviceEasyCoded.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+
+// מה שקורה כאן זה שפשוט כל הקוד הקשה שיש בקובץ השני ה-רקוואייר עושה לבד !!
+const dataResult = require('./data.json');
+console.log('dataResult', dataResult);
